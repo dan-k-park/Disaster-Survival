f17691274836b15e2730bf39ddbb7d95038526cf
Daniel Park <parkdk96@gmail.com>
2019-11-12 15:48:09 -0800

    merged new games, delete games, new users

--- disaster-survival-frontend/src/index.js
+++ disaster-survival-frontend/src/index.js
@@ -7,17 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
   getGames();
 })
 
-// const initializeSignInForm = () => {
-//   let signInForm = document.getElementById('signin_form')
-
-//   signInForm.addEventListener('submit', (ev) => {
-//     ev.preventDefault();
-
-//     let username = ev.target.username_input.value;
-//   })
-//   //document.getElementById('authentication').classList.add('hidden')
-// }
-
 const addUser = () => {
   let newUserForm = document.getElementById('signup_form')
 
@@ -50,8 +39,11 @@ const getGames = () => {
 
 const displayGames = (games) => {
   let gameList = document.getElementById('games')
-  gameList.classList.remove('hidden')
   let ul = document.createElement('ul');
+
+  let gameForm = document.getElementById("new_game_form")
+  let game_input = document.getElementById("game_input")
+
   games.forEach(game => {
     let li = document.createElement('li')
     li.textContent = game.game_name
@@ -64,7 +56,42 @@ const displayGames = (games) => {
       deleteGame(e, game);
     };
   });
+
   gameList.appendChild(ul)
+
+  gameForm.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+    
+    fetch(GAMES_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        game_name: game_input.value,
+        score: 1,
+        user_id: 1,
+        health: 100,
+        turn: 1,
+        status: true
+      })
+    })
+    .then(res => res.json())
+    .then(game => {
+      let li = document.createElement('li');
+      li.textContent = game.game_name;
+
+      let deleteBtn = document.createElement('button');
+      deleteBtn.innerText = 'Delete Game';
+      li.appendChild(deleteBtn)
+      deleteBtn.onclick = e => {
+        deleteGame(e, game);
+      }
+    
+      ul.appendChild(li)
+    })
+  })
 }
 
 const deleteGame = (e, game) => {
